676bd672e26990194d9c93872d2c691f95e26188
majorebola <edoardo.davini@gmail.com>
2017-06-28 11:57:16 +0200

    a lot of important things

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get('/', function(req, res, next) {
 	let data = fs.readFileSync(path.join(__dirname, '..', 'data', 'planets.json'));
 	let params = {
-		title: "Sibice: Stars In Binary InterConnection Effect",
+		showHeader: true,
 		planets: JSON.parse(data),
 		enableDesc: false
 	};
@@ -16,7 +16,7 @@ router.get('/', function(req, res, next) {
 	// res.json(planets);
 });
 
-router.get('/:planetId', function (req, res, next) {
+router.get('/planet/:planetId', function (req, res, next) {
 	let data = fs.readFileSync(path.join(__dirname, '..', 'data', 'planets.json'));
 	let planets = JSON.parse(data);
 	let planet = planets[req.params.planetId];
@@ -26,6 +26,17 @@ router.get('/:planetId', function (req, res, next) {
 		enableDesc: true
 	};
 	res.render('singlePlanet', params)
+});
+
+router.get('/classification', function (req, res, next) {
+	let data = fs.readFileSync(path.join(__dirname, '..', 'data', 'legend.json'));
+	let json = JSON.parse(data);
+	let params = {
+		showHeader: true,
+		lifeSustain: json.lifeSustain,
+		types: json.type
+};
+	res.render('classification', params);
 });
 
 module.exports = router;
